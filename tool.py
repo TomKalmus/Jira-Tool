79fbe3ea79a0c693f44c420586e82bb3e95521ef
TomKalmus <tkalm@eg.dk>
2024-10-30 11:39:04 +0100

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,1 +1,19 @@
+import requests
+class APIClient:
+  def __init__(self, base_url, api_token):
+    self.base_url = base_url
+    self.api_token = api_token
 
+  def get_data(self):
+    headers = {
+        "x-api-token": self.api_token
+    }
+    response = requests.get(self.base_url, headers=headers)
+    print("Response from server:", response.text)
+
+def main():
+  client = APIClient("https://example.com", "qk9KiCnUAMJitZwYkcR8")
+  client.get_data()
+
+if __name__ == "__main__":
+    main()
